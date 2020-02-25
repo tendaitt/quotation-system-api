434d4ae33edd25da42057a4538f58502773f2cf7
tendaitt <970497+tendaitt@users.noreply.github.com>
2020-02-25 13:53:18 +0200

    Add placeholder functions for login and logout

--- services/server/src/users/views.py
+++ services/server/src/users/views.py
@@ -1,3 +1,29 @@
+def login(username, password):  # noqa: E501
+    """Logs users into the system
+
+     # noqa: E501
+
+    :param username: The username for login
+    :type username: str
+    :param password: The password for login
+    :type password: str
+
+    :rtype: str
+    """
+    return 'do some magic!'
+
+
+def logout():  # noqa: E501
+    """Logs out current logged in user session
+
+     # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
 def new_user(body):  # noqa: E501
     """Register a new user
 
