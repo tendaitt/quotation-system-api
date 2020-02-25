156016f8a125a785a93a57e89a435175aa10535f
tendaitt <970497+tendaitt@users.noreply.github.com>
2020-02-25 13:57:47 +0200

    Resolve linting issue

--- services/server/config.py
+++ services/server/config.py
@@ -15,7 +15,8 @@ class Config(object):
 
     GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
     GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
-    GOOGLE_DISCOVERY_URL = ("https://account.google.com/.well-known/openid-configuration")
+    GOOGLE_DISCOVERY_URL = \
+        "https://account.google.com/.well-known/openid-configuration"
     FACEBOOK_APP_ID = os.environ.get("FACEBOOK_APP_ID", None)
 
 
