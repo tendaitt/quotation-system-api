9562fa1f07ba95c6830af238b0737f791c7e3908
tendaitt <970497+tendaitt@users.noreply.github.com>
2020-02-27 11:46:57 +0200

    Remove test data set up

--- services/server/src/tests/conftest.py
+++ services/server/src/tests/conftest.py
@@ -14,55 +14,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 TEST_DB = "test"
 
 
-def load_customer_data():
-
-    conn = psycopg2.connect(
-        host='localhost',
-        database=TEST_DB,
-        user='postgres'
-    )
-
-    c = conn.cursor()
-
-    with open(os.path.join(basedir, 'customer_data.csv'), 'r') as my_file:
-
-        reader = csv.reader(my_file)
-
-        for row in reader:
-            c.execute(
-                "INSERT INTO customers\
-                    (first_name, last_name, email, phone_number)\
-                        VALUES (%s, %s, %s, %s);",
-                row
-            )
-
-    conn.commit()
-
-
-def load_product_data():
-
-    conn = psycopg2.connect(
-        host='localhost',
-        database=TEST_DB,
-        user='postgres'
-    )
-
-    c = conn.cursor()
-
-    with open(os.path.join(basedir, 'product_data.csv'), 'r') as my_file:
-
-        reader = csv.reader(my_file)
-
-        for row in reader:
-            c.execute(
-                "INSERT INTO products (name, description, quantity, price)\
-                    VALUES (%s, %s, %s, %s);",
-                row
-            )
-
-    conn.commit()
-
-
 @pytest.fixture(scope='module')
 def test_app():
     app = create_app()
@@ -88,9 +39,6 @@ def test_db(test_app):
 
         db.init_app(test_app)
         db.create_all()
-
-        load_customer_data()
-        load_product_data()
 
         yield db
 
