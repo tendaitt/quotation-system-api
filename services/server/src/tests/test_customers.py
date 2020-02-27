06887e9dc7e0e45213c60b4d40838ba1ab9e0453
tendaitt <970497+tendaitt@users.noreply.github.com>
2020-02-27 09:54:54 +0200

    Implement test_add_new_customer_when_none_exists()

--- services/server/src/tests/test_customers.py
+++ services/server/src/tests/test_customers.py
@@ -2,8 +2,22 @@ def test_add_new_customer_to_database():
     pass
 
 
-def test_add_new_customer_when_none_exists():
-    pass
+def test_add_new_customer_when_none_exists(test_client, test_db):
+    new_customer = {
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'email': 'john@doe.com',
+        'phone_number': '(000)000-0000'
+    }
+
+    response = test_client.post(
+        '/v1/customer',
+        json=new_customer,
+        content_type='application/json'
+    )
+
+    assert response.status_code == 201
+    assert response.data == b'New customer was succesfully created'
 
 
 def test_cannont_add_duplicate_customer():
