3186ffce1337f524d049d8cf7d5e15615c0bfea7
tendaitt <970497+tendaitt@users.noreply.github.com>
2020-02-27 11:04:10 +0200

    Update script to account for change in app initialization

--- services/server/db_create.py
+++ services/server/db_create.py
@@ -1,7 +1,9 @@
-from src import create_app, db
+from src import create_app
+from src.database import db
 
 # create the database and tables
 app = create_app()
+db.init_app(app)
 
 with app.app_context():
     db.create_all()
