d5a11cdc77d10bf9dc043d9c986e657d5dad26a7
tendaitt <970497+tendaitt@users.noreply.github.com>
2020-03-03 12:12:07 +0200

    Implement test_add_new_product_to_database()

--- services/server/src/tests/test_inventory.py
+++ services/server/src/tests/test_inventory.py
@@ -1,4 +1,36 @@
-def test_add_product():
+from src.models import Product
+
+
+def test_add_new_product_to_database(test_db):
+    new_product = Product(
+        name='Tables',
+        description='10 seater',
+        quantity=5,
+        price=500
+    )
+
+    test_db.session.add(new_product)
+    test_db.session.commit()
+
+    product = Product.query\
+        .filter(Product.name == 'Tables')\
+        .filter(Product.description == '10 seater')\
+        .filter(Product.quantity == 5)\
+        .filter(Product.price == 500)\
+        .one_or_none()
+
+    assert product is not None
+
+
+def test_add_new_product_when_none_exists():
+    pass
+
+
+def test_add_new_duplicate_customer():
+    pass
+
+
+def test_add_product_with_existing_name():
     pass
 
 
