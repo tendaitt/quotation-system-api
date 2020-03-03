3888f155c0d6b71021dfdfa834d28982f24c19b8
tendaitt <970497+tendaitt@users.noreply.github.com>
2020-03-03 12:17:53 +0200

    Implement test_add_new_product_when_none_exists()

--- services/server/src/tests/test_inventory.py
+++ services/server/src/tests/test_inventory.py
@@ -22,8 +22,22 @@ def test_add_new_product_to_database(test_db):
     assert product is not None
 
 
-def test_add_new_product_when_none_exists():
-    pass
+def test_add_new_product_when_none_exists(test_client, test_db):
+    new_product = {
+        'name': 'Tables',
+        'description': '10 seater',
+        'quantity': 5,
+        'price': 500
+    }
+
+    response = test_client.post(
+        '/v1/inventory',
+        json=new_product,
+        content_type='application/json'
+    )
+
+    assert response.status_code == 201
+    assert response.data == b'New product successfully created'
 
 
 def test_add_new_duplicate_customer():
