390d9eb45e975c1450bb40f5bfabe8176a5777d7
tendaitt <970497+tendaitt@users.noreply.github.com>
2020-03-04 11:06:23 +0200

    Implement test_add_new_quote_to_database()

--- services/server/src/tests/test_quotes.py
+++ services/server/src/tests/test_quotes.py
@@ -1,4 +1,44 @@
-def test_create_quote():
+from src.models import Quote, QuoteItem, Transaction, Customer
+
+
+def test_add_new_quote_to_database(test_db):
+    new_customer = Customer(
+        first_name='John',
+        last_name='Doe',
+        email='john@doe.com',
+        phone_number='(000) 000-0000'
+    )
+
+    test_db.session.add(new_customer)
+    test_db.session.commit()
+
+    new_quote = Quote(
+        customer_id=1,
+        date='01/01/2020',
+        description='New quote'
+    )
+
+    test_db.session.add(new_quote)
+    test_db.session.commit()
+
+    quote = Quote.query\
+        .filter(Quote.customer_id == 1)\
+        .filter(Quote.date == '01/01/2020')\
+        .filter(Quote.description == 'New quote')\
+        .one_or_none()
+
+    assert quote is not None
+
+
+def test_add_new_quote_item_to_database(test_db):
+    pass
+
+
+def test_add_new_transaction_to_database(test_db):
+    pass
+
+
+def test_add_new_quote():
     pass
 
 
