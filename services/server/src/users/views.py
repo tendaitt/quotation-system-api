b75f470cf4eeaa1f8d8f3e06025375bd3f4e4b01
tendaitt <970497+tendaitt@users.noreply.github.com>
2020-03-09 16:01:43 +0200

    Implement register()

--- services/server/src/users/views.py
+++ services/server/src/users/views.py
@@ -1,4 +1,8 @@
-from src import login_manager
+from connexion import request
+from flask import make_response, jsonify
+from sqlalchemy.exc import IntegrityError
+
+from src import login_manager, bcrypt, db
 from src.models import User
 
 
@@ -15,5 +19,40 @@ def logout():
     return 'do some magic!'
 
 
-def new_user(body):
-    return 'do some magic!'
+def register(body):
+    if request.is_json:
+        username = body.get('username')
+        email = body.get('email')
+        password = bcrypt.generate_password_hash(body.get('password'))
+        image_url = body.get('image_url')
+
+        new_user = User(
+            username=username,
+            email=email,
+            password=password,
+            image_url=image_url
+        )
+
+        existing_user = User.query\
+            .filter(User.username == username)\
+            .filter(User.email == email)\
+            .one_or_none()
+
+        if existing_user is None:
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+                return make_response(
+                    'New user was successfully created',
+                    201
+                )
+            except IntegrityError:
+                return make_response(
+                    'A user with the same credentials already exists',
+                    409
+                )
+        else:
+            return make_response(
+                f'User, {username}, already exists',
+                409
+            )
